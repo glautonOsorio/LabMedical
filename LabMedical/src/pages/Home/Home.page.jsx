bc302b005451fc37c138fd40017fdbe08fa2be5a
Glauton Osório <glautonleao@gmail.com>
2023-07-18 17:50:58 -0300

    sidebar skeleton

--- LabMedical/src/pages/Home/Home.page.jsx
+++ LabMedical/src/pages/Home/Home.page.jsx
@@ -1,16 +1,37 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../contexts/auth.context";
 import { Navigate } from "react-router-dom";
+import "./Home.style.css";
 
 export const HomePage = () => {
   const { auth } = useContext(AuthContext);
   const loggedUser = localStorage.getItem("Logged");
 
+  const [abrir, setAbrir] = useState();
+
+  function abrirPokebola() {
+    if (abrir) {
+      setAbrir();
+    } else {
+      setAbrir("18rem");
+    }
+  }
+
   const render = () => {
     return (
-      <>
-        <p>HomePage is Render</p>
-      </>
+      <div>
+        <div className="sidenav" style={{ width: abrir }}>
+          <a className="closebtn" onClick={abrirPokebola}>
+            &times;
+          </a>
+          <a>About</a>
+          <a>Services</a>
+          <a href="#">Clients</a>
+          <a href="#">Contact</a>
+        </div>
+
+        <span onClick={abrirPokebola}>open</span>
+      </div>
     );
   };
   return loggedUser || auth.isLogged ? render() : <Navigate to="/login" />;
